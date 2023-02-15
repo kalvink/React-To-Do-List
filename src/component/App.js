9706c793639838b3ee57d84045f22284846082bb
Kalvin <kalvin.kao@hotmail.com>
2023-02-15 11:44:10 -0500

    update

--- src/component/App.js
+++ src/component/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import TodoForm from './TodoForm';
 import TodoItem from './TodoItem';
+import './App.css';
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
 
   const addTodo = text => {
-    const newTodos = [...todos, { text }];
+    const newTodos = [...todos, { text, completed: false }];
     setTodos(newTodos);
   };
 
@@ -16,12 +17,24 @@ function TodoList() {
     setTodos(newTodos);
   };
 
+  const toggleTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
   return (
     <div>
       <TodoForm addTodo={addTodo} />
       <ul>
         {todos.map((todo, index) => (
-          <TodoItem key={index} index={index} todo={todo} removeTodo={removeTodo} />
+          <TodoItem
+            key={index}
+            index={index}
+            todo={todo}
+            removeTodo={removeTodo}
+            toggleTodo={toggleTodo}
+          />
         ))}
       </ul>
     </div>
--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './TodoItem.css';
 
-function TodoItem({ todo, index, removeTodo }) {
+function TodoItem({ todo, index, removeTodo, toggleTodo }) {
+  const completedStyle = {
+    textDecoration: 'line-through'
+  };
+
   return (
     <li>
-      {todo.text}
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => toggleTodo(index)}
+      />
+      <span style={todo.completed ? completedStyle : null}>
+        {todo.text}
+      </span>
       <button onClick={() => removeTodo(index)}>Remove</button>
     </li>
   );
